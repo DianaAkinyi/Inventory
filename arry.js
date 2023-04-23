c8886c966fa1d714ce0d5fcf6cd01b99d1506463
DianaAkinyi <dianaakinyiowiti@gmail.com>
2023-04-23 23:25:17 +0300

    functionss

--- arry.js
+++ arry.js
@@ -1,23 +1,24 @@
 
-function add(keys, values) {
-    const array3 = {};
+// Create an array containing the names of all items in the inventory maximum of 10.
+const array =["Cereals","Fruits","Bread","Cheese","Eggs","Rice","Pasta","Snacks","Drinks","tomato"]
+// Create a separate array with the corresponding stock quantities of each item maximum of 10.
+const array2 =[5,10,20,30,40,50,60,70,80,90]
+//creating an inventory
+const array3 = {};
   
-     keys.forEach((element, index) => {
-      array3[element] = values[index];
+     array.forEach((element, index) => {
+      array3[element] = array2[index];
      });
-  
-    return array3;
-  }
-  
-  const array =["Cereals","Fruits","Bread","Cheese","Eggs","Rice","Pasta","Snacks","Drinks","tomato"]
-  const array2 =[5,10,20,30,40,50,60,70,80,90]
-  const array3 = add(array, array2);
-   console.log(array3);
-   array3.banana = 45
-   console.log(array3);
-//Write a function to update the stock quantity of an existing item.
- function update(array3){
-    // return array3.Eggs
+  console.log(array3)
+//   Write a function to add a new item to the inventory, updating both arrays.
+function adding(){
+    array3["banana"]=5
+
+console.log(array3)
+}
+adding()
+// Write a function to update the stock quantity of an existing item.
+function update(array3){
     
     array3.Eggs=90
     return array3
@@ -25,7 +26,7 @@ function add(keys, values) {
 
 console.log(update(array3))
 
-//Write a function to calculate the total number of items in the inventory.
+// Write a function to calculate the total number of items in the inventory.
 function total (){
     const values = Object.values(array3);
 
@@ -36,26 +37,20 @@ function total (){
 console.log(sum); 
 }
 total()
+// Write a function to find the item with the lowest stock quantity.
+ 
 
-//Write a function to find the item with the lowest stock quantity.
-// function finding(array3){
-// // // console.log(Math.min(array3.values));
-// console.log(Math.min(...array3.values));
-// }
-// finding()
-
-let keys=Object.keys(array3)
-let minkey=keys[0]
-for(let i = 1; i < keys.length; i++){
-    let minimum = array3[minkey];
-    let value = array3[keys[i]];
-    if(minimum > value) minkey = keys[i];
-}
-
-console.log(minkey, array3[minkey]);
-
-
-
+    let keys=Object.keys(array3)
+    let minkey=keys[0]
+    for(let i = 1; i < keys.length; i++){
+        let minimum = array3[minkey];
+        let value = array3[keys[i]];
+        if(minimum > value) minkey = keys[i];
+    }
+    
+    console.log(minkey, array3[minkey]);
+    
+    
 
 
 
